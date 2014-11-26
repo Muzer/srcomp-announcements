88a64542d8feca6e990947f5053d34895bb00b1d
Murray Colpman <muzer@tim32.org>
2014-11-26 22:26:14 +0000

    Fix bug caused by dynamic typing in combination with stupidity

--- event_schedule.py
+++ event_schedule.py
@@ -7,6 +7,7 @@ import queue
 import datetime
 import dateutil.parser
 import pytz
+import traceback
 
 def go(url, notification_requests, arena):
     events = queue.Queue()
@@ -14,44 +15,35 @@ def go(url, notification_requests, arena):
     serverthread = threading.Thread(target=get_events, args=(url, notification_requests, arena, timers, events))
     serverthread.start()
     while True:
-        print("Blocking on getting")
         yield events.get()
-        print("Finished getting")
 
 
 def wait_for_event(name, match_data, timers, events):
     timers.remove(threading.current_thread())
-    print("Putting")
     events.put((name, match_data))
-    print("Finished putting")
     
 
 def get_events(url, notification_requests, arena, timers, events):
     messages = sseclient.SSEClient(url)
-    print("Blocking on message")
     for msg in messages:
-        print("Finished blocking")
         if msg.event == "match":
             for t in timers:
                 t.cancel()
                 timers.remove(t)
             match_data = json.loads(msg.data)
             try:
-                arena, = filter(lambda x : x["arena"] == arena, match_data)
-            except ValueError:
+                arena_data, = filter(lambda x : x["arena"] == arena, match_data)
+            except ValueError as steve:
                 continue
             for time, key in [
-                    (arena["times"]["game"]["start"], "game-start"),
-                    (arena["times"]["game"]["end"], "game-end"),
-                    (arena["times"]["period"]["start"], "period-start"),
-                    (arena["times"]["period"]["end"], "period-end")]:
+                    (arena_data["times"]["game"]["start"], "game-start"),
+                    (arena_data["times"]["game"]["end"], "game-end"),
+                    (arena_data["times"]["period"]["start"], "period-start"),
+                    (arena_data["times"]["period"]["end"], "period-end")]:
                 for delta, name in notification_requests.get(key, []):
-                    print((time, key, delta, name))
                     notification_delta = (dateutil.parser.parse(time)+datetime.timedelta(seconds=delta)) - datetime.datetime.now(pytz.utc)
                     if notification_delta < datetime.timedelta():
                         continue
-                    print("running thread")
                     t = threading.Timer(notification_delta.total_seconds(), wait_for_event, args=(name, match_data, timers, events))
                     timers.append(t)
                     t.start()
-        print("Blocking on message")
